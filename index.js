817484a00d5ef63582b24b3ef6447578b3c0a379
Robert3 <robertbls16@gmail.com>
2023-03-28 13:27:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById("btn").addEventListener("click", function(){
+
+fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        document.getElementById("activity").textContent = data.activity
+    })
+    })
